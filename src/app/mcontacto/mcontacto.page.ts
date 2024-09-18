1b9e3d415c31786a8cdd4e056226686080eae553
Ariel Llerena <135856134+cuatrix2@users.noreply.github.com>
2024-09-17 20:26:59 -0500

    actualziacon

--- src/app/mcontacto/mcontacto.page.ts
+++ src/app/mcontacto/mcontacto.page.ts
@@ -8,47 +8,59 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./mcontacto.page.scss'],
 })
 export class McontactoPage implements OnInit {
-datos:any=[];
-cod_contacto: string="";
-txt_nombre:string="";
-txt_apellido:string="";
-txt_telefono:string="";
-txt_email:string="";
+  datos: any = [];
+  cod_contacto: string = '';
+  txt_nombre: string = '';
+  txt_apellido: string = '';
+  txt_telefono: string = '';
+  txt_email: string = '';
 
-  constructor(public navCtrl: NavController, public servicio: AccesoService) { 
-    this.servicio.getSession("cod_contacto").then((res:any)=>{
-      this.cod_contacto=res;
+  constructor(public navCtrl: NavController, public servicio: AccesoService) {
+    this.servicio.getSession('cod_contacto').then((res: any) => {
+      this.cod_contacto = res;
       //funcion para llamar datos
       this.cargar_datos();
     });
   }
 
-  ngOnInit() {
-  }
-  cargar_datos(){
-    let datos={
-      accion: "dcontacto",
-      cod_contacto:this.cod_contacto
+  ngOnInit() {}
+  cargar_datos() {
+    let datos = {
+      accion: 'dcontacto',
+      cod_contacto: this.cod_contacto,
     };
-    this.servicio.postData(datos).subscribe((res:any)=>{
-      if(res.estado){
-        datos=res.datos;
-        this.txt_nombre=res.datos.nombre;
-        this.txt_apellido=res.datos.apellido;
-        this.txt_telefono=res.datos.telefono;
-        this.txt_email=res.datos.email;
-
-      }else{
+    this.servicio.postData(datos).subscribe((res: any) => {
+      if (res.estado) {
+        datos = res.datos;
+        this.txt_nombre = res.datos.nombre;
+        this.txt_apellido = res.datos.apellido;
+        this.txt_telefono = res.datos.telefono;
+        this.txt_email = res.datos.email;
+      } else {
         this.servicio.showToast(res.mensaje);
       }
     });
   }
-
-  actualizar(){
-
-   
+  actualizar() {
+    let datos = {
+      accion: 'acontacto',
+      cod_contacto: this.cod_contacto,
+      nombre: this.txt_nombre,
+      apellido: this.txt_apellido,
+      telefono: this.txt_telefono,
+      email: this.txt_email,
+    };
+    console.log(datos);
+    this.servicio.postData(datos).subscribe((res: any) => {
+      if (res.estado == true) {
+        this.servicio.showToast(res.mensaje);
+        this.navCtrl.navigateRoot('/contactos');
+      } else {
+        this.servicio.showToast(res.mensaje);
+      }
+    });
   }
-  cancelar(){
+  cancelar() {
     this.navCtrl.navigateRoot('/contactos');
-  }
+  }
 }
